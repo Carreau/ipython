c1302089c419be79a20f708e1408835f4bcb07a7
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-10-20 19:11:10 -0700

    reformat with darker

--- IPython/utils/tests/test_openpy.py
+++ IPython/utils/tests/test_openpy.py
@@ -30,9 +30,8 @@ def test_source_to_unicode():
     with io.open(nonascii_path, "rb") as f:
         source_bytes = f.read()
     assert (
-        openpy.source_to_unicode(source_bytes,
-                                 skip_encoding_cookie=False).splitlines() ==
-        source_bytes.decode("iso-8859-5").splitlines()
+        openpy.source_to_unicode(source_bytes, skip_encoding_cookie=False).splitlines()
+        == source_bytes.decode("iso-8859-5").splitlines()
     )
 
     source_no_cookie = openpy.source_to_unicode(source_bytes, skip_encoding_cookie=True)
