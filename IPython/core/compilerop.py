4311fa9316578ace1415e5cee9c57616c410ad5a
M Bussonnier <bussonniermatthias@gmail.com>
2025-11-02 21:12:16 +0100

    allow interpreted classes

--- IPython/core/compilerop.py
+++ IPython/core/compilerop.py
@@ -66,6 +66,10 @@ def code_name(code, number=0):
 # Classes and functions
 #-----------------------------------------------------------------------------
 
+from mypy_extensions import mypyc_attr
+
+
+@mypyc_attr(allow_interpreted_subclasses=True)
 class CachingCompiler(codeop.Compile):
     """A compiler that caches code compiled from interactive statements.
     """
